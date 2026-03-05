dfe2b951be9c4709bce302d44a5c8f9ab1af2e80
hw-erl-lnaylor <luke.naylor@h-partners.com>
2026-03-05 11:20:39 +0000

    Move assets into source instead of having in static section

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,16 +2,17 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.string(),
-    authors: z.array(z.string()),
-    repoLink: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-    descriptionVideo: z.string().optional(),
-    descriptionImage: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.string(),
+      authors: z.array(z.string()),
+      repoLink: z.string().optional(),
+      tags: z.array(z.string()).optional(),
+      descriptionVideo: z.string().optional(),
+      descriptionImage: image().optional(),
+    }),
 });
 
 const news = defineCollection({
